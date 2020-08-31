6a556e0b3a7a8ea6dd168050b54b501aeadd7f8a
Ryan Xin <xinpf0715@gmail.com>
2020-08-31 10:50:04 +1000

    message create & read finished

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,6 @@ class MessagesController < ApplicationController
     # if @message.persisted?
     @current_user.sent_messages << @message
     # end
-    # raise "hell"
     redirect_to(message_path(message_params[:recipient_id]))
   end
 
@@ -18,17 +17,14 @@ class MessagesController < ApplicationController
 
     @recipient_id = @messaged_friends.first.id
     
-    # railse "hell"
   end
   
   def show
     @current_user = User.first
-    # railse "hell"
     @recipient_id = params[:id]
     @conversation_messages = (Message.where(["sender_id = ? and recipient_id = ?", @current_user.id, @recipient_id]) + Message.where(["sender_id = ? and recipient_id = ?", @recipient_id, @current_user.id])).sort_by(&:created_at)
 
     @message = Message.new
-    # redirect_to(messages_path)
   end
 
   def edit
