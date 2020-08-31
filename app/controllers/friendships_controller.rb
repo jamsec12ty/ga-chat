691f7a444777c9cd0efa9d7987b7100dfca02644
sarop-bajra <sarop_bajra@hotmail.com>
2020-08-31 10:49:24 +1000

    Friendship CRUD

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,52 @@
+class FriendshipsController < ApplicationController
+
+  before_action :check_if_logged_in
+
+  # CREATE
+
+  def new
+    @friendship = Friendship.new
+    @users = User.all
+  end
+
+  def create
+    Friendship.create friendship_params
+    redirect_to friendships_path
+  end
+
+  # READ
+
+  def index
+    @friendships = Friendship. Friendship.all
+  end
+
+  def show
+    @friendship = Friendship.find params[:id]
+  end
+
+  # UPDATE
+
+  def edit
+    @friendship = Friendship.find params[:id]
+  end
+
+  def update
+    friendship = Friendship.find params[:id]
+    Friendship.update friendship_params
+
+    redirect_to( friendship_path(params[:id]) )
+  end
+
+  # DELETE
+
+  def destroy
+    Friendship.destroy params[:id]
+    redirect_to friendships_path
+  end
+
+  private
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id, :status)
+
+  end
+end # 
---
+++ test/controllers/friendships_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class FriendshipsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get friendships_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get friendships_create_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get friendships_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get friendships_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get friendships_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get friendships_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get friendships_destroy_url
+    assert_response :success
+  end
+
+end
