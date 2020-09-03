386bf65ce62f0ca659cf9f39c5ce06558045782e
Ryan Xin <xinpf0715@gmail.com>
2020-09-03 15:04:51 +1000

    update message crud

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
       let query = JSON.parse(localStorage.getItem('id'));
       console.log(query);
       recipientId = query;
+
       $.getJSON(`messages/show/${query}`)
         .done(data => {
           console.log(data);
@@ -97,6 +98,7 @@ $(document).ready(function () {
   $('.message_list_item').on('click', (e) => {
     $('.message_window').empty();
     $('.message_send_wrapper').show();
+    e.currentTarget.className += ' selected';
     const query = e.currentTarget.id;
     // Save the last clicked user id, for using when sending message
     recipientId = query; 
